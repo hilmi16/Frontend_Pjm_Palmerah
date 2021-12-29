08ee3cbfd0de0c63b4ce24e94528e71b774d11d9
hilmi16 <57147552+hilmi16@users.noreply.github.com>
2021-12-30 01:30:26 +0700

    Add files via upload

---
+++ lib/custom/preference_profile.dart
@@ -0,0 +1,11 @@
+class Preference {
+  //ini preference untuk mendukung fitur session
+  static String id = "id";
+  static String email = "email";
+  static String phone = "phone";
+  static String namaLengkap = "namaLengkap";
+  static String tanggalDibuat = "tanggalDibuat";
+  static String level = "level";
+  static String kode = "kode";
+  static String login = "login";
+}
